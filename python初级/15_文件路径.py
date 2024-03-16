191d42d5cdd653504a83b550a3711dc648309dfb
Geistzzz <lilizhangchi@gmail.com>
2024-03-17 00:55:05 +0800

    daily

--- python初级/15_文件路径.py
+++ python初级/15_文件路径.py
@@ -4,4 +4,30 @@
 # @File ： 15_文件路径.py
 # @Software: PyCharm
 
+# 获取当前工作目录路径
+# import os
+#
+# print(os.getcwd())  # 获取当前工作目录路径
+# print(os.path.abspath('.'))  # 获取当前工作目录路径
+#
+# weight_path = '/weights.txt'
+# current_work_dir = os.path.dirname(__file__)  # 当前文件所在的目录
+#
+# weight_path = os.path.join(current_work_dir, weight_path)  # 再加上它的相对路径，这样可以动态生成绝对路径
+#
+# print(weight_path)
+
+# 创建文件夹
+
+import os
+
+# 操作的文件夹路径
+
+operate_path = r"/Users/mac/Documents/Rustproject"
+for root, dirs, files in os.walk(operate_path):
+    print('root:', root)
+    print('dirs:', dirs)
+    print('files:', files)
+    print('\n')
+
 
