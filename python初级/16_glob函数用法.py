1d1cd8e3595a33ff463aa0959cf0ef8ae9b95405
Geistzzz <lilizhangchi@gmail.com>
2024-03-18 00:05:48 +0800

    daily

---
+++ python初级/16_glob函数用法.py
@@ -0,0 +1,22 @@
+"""
+《邢不行-2020新版|Python数字货币量化投资课程》
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+抓取数字货币交易所的数据，构建数据库
+"""
+
+import os
+
+# ===保存数据到文件
+# 创建交易所文件夹
+path = os.path.join(path, exchange.id)
+if os.path.exists(path) is False:
+    os.mkdir(path)
+
+
+
+
